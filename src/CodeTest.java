7adc0ba893c102421f37113599bc189478cf219b
Jay Bommes <jay.bommes@gmail.com>
2026-03-25 14:46:43 +0100

    Erste Codeänderung

--- src/CodeTest.java
+++ src/CodeTest.java
@@ -1,5 +1,7 @@
 public class CodeTest {
     public static void main(String[] args) throws Exception {
         System.out.println("CodeTest 2!");
+
+        System.out.println("CodeTest 3!");
     }
 }
